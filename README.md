e0be901b1cc24a3c34e95305407a2b9663295f0c
yangjunjun <ggiiss@qq.com>
2022-07-27 09:53:05 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-x3qcrq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-x3qcrq)
---
+++ src/components/TodoApp.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="TodoApp">
+    <h1>todo app</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoApp',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped></style>
